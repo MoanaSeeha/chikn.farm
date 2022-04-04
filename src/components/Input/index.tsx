3f1a9f7d1ab7abdae74f1bfd8e022bfad6416d56
Prometheus <96538997+MoanaSeeha@users.noreply.github.com>
2022-04-04 15:15:11 +0800

    completing lpfarm

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,22 +5,27 @@ import Icons from "../Icons";
 
 type Props = {
   className?: string,
+  inputClass?: string,
   onChange: (i: number) => void;
+  value?: string,
+  type?: string
 }
 
 
 
 const Input: FC<Props> = (props: Props) => {
 
-  const {className} = props;
+  const {className, value, inputClass, type} = props;
   const[focus,onFocus] = useState(false);
 
   return (
       <div className={`inline-block mb-3 xl:w-48 min-w-[200px] relative cursor-pointer z-50 ${className}`}>
         <input 
-          className={`${focus?'selectbox ':''}block w-full px-3 py-1.5 text-sm font-normal bg-[#222343] bg-clip-padding bg-no-repeat border border-solid border-[#3b3d76] rounded-lg transition ease-in-out m-0 `}
+          type={type}
+          className={`${focus?'selectbox ':''}block w-full px-3 py-1.5 text-sm font-normal bg-[#222343] bg-clip-padding bg-no-repeat border border-solid border-[#3b3d76] rounded-lg transition ease-in-out m-0 ${inputClass}`}
           onFocus={() =>onFocus(true)}
           onBlur={() =>onFocus(false)}
+          value={value}
         >
         </input>
         
--- src/pages/farmland/lpfarm/EggAvax.tsx
+++ src/pages/farmland/lpfarm/EggAvax.tsx
@@ -6,23 +6,21 @@ import { Board } from '../../../components/Board';
 import SelectBox, {ISelectItem} from '../../../components/SelectBox';
 import Input from '../../../components/Input';
 import Icons from '../../../components/Icons';
-
+import { RoundedButton } from '../../../components/Button';
 
 const EggAvax = () => {
 
   const [toggleStatus, toggle] = useState(true);
-  const seletBoxData:ISelectItem[] = [
-    {title: 'Largest Size', value: 0},
-    {title: 'Smallest Size', value: 1},
-    {title: 'Highest Farm', value: 2}
-  ];
+  const [selectedTab, changeTab] = useState(true);
 
   return (
-      <Board className={`relative ${toggleStatus?'pb-0': ''}`}>
-        <div onClick={() => toggle(!toggleStatus)} className={`${toggleStatus?'rounded-br-lg rounded-bl-lg ':''}items-center cursor-pointer absolute bg-[#222343] rounded-tr-lg rounded-tl-lg w-full top-0 flex justify-between px-5 py-2`}>
+      <Board className={`relative pb-0`}>
+        <div onClick={() => toggle(!toggleStatus)} className={`${!toggleStatus?'rounded-br-lg rounded-bl-lg ':''}items-center cursor-pointer absolute bg-[#222343] rounded-tr-lg rounded-tl-lg w-full top-0 flex justify-between px-5 py-2`}>
           <div className='flex space-x-4 items-center'>
-            <img src={`${process.env.PUBLIC_URL}/img/FARM_TOKEN-7d723a9ae0ec9e1801046749cf0faa94.png`} alt="" className=' md:w-12 md:h-12 w-10 h-10'/>
-            <div className=' md:text-3xl text-2xl font-black tracking-widest'>FARMLAND</div>
+          <img src={`${process.env.PUBLIC_URL}/img/eggtoken.svg`} alt="" className='w-8 h-8'/>
+            <div className='text-2xl font-black tracking-widest'>$EGG /</div>
+            <img src={`${process.env.PUBLIC_URL}/img/download.svg`} alt="" className='w-8 h-8'/>
+            <div className='text-2xl font-black tracking-widest'>$AVAX</div>
           </div>
           <div className=' text-center'>
             {!toggleStatus?<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -34,7 +32,32 @@ const EggAvax = () => {
         </div>
         <SlideDown className={'my-dropdown-slidedown'}>
           {toggleStatus ? 
-            <></>
+            <div className='px-3 flex md:flex-row flex-col space-x-0 md:space-x-5 py-5'>
+              <div className='flex flex-col md:basis-2/5 px-5 bg-[#222343] rounded-lg py-8'>
+                <div className='flex flex-row justify-start space-x-5 text-lg'>
+                  <div className={`${selectedTab?'chikn-red ':''} cursor-pointer transition-all`} onClick={() => changeTab(true)}>Deposite</div>
+                  <div className={`${!selectedTab?'chikn-red ':''} cursor-pointer transition-all`} onClick={() => changeTab(false)}>Withdraw</div>
+                </div>
+                <div className='flex flex-col justify-center mt-4 space-y-5'>
+                    <div className='flex justify-center py-2 px-5 bg-[#df3f3d] rounded-lg text-[#f2f2f2] text-base'>
+                      TRADER JOE <img src={`${process.env.PUBLIC_URL}/img/JLP_Logo-482a1e1547fafda8e8b152d09098d3e3.png`} alt='' className=' w-6 h-6'/>
+                    </div>
+                    <a href="#" className='flex justify-center inline-block chikn-red underline decoration-[#df3f3d]'>Get JLP tokens here</a>
+                    <div className='flex justify-center text-4xl font-extrabold'>0.000000</div>
+                    <div className='flex justify-center text-xl'>TOTAL LP WALLET BALANCE</div>
+                    <div className='flex flex-row'>
+                      <Input value='0' inputClass=' rounded-r rounded-xl' className='basis-9/12 min-w-0' onChange={() => {}}/>
+                      <Input inputClass=' rounded-l rounded-xl cursor-pointer' className='basis-3/12 min-w-0' value='Max' type='button' onChange={() => {}}/>
+                    </div>
+                    <RoundedButton title='Approve' disabled/>
+                    <RoundedButton title='Deposite'/>
+                </div>
+              </div>
+              <div className='flex flex-col justify-center text-center space-y-5 md:basis-3/5 px-10 chikn-red bg-[#222343] rounded-lg py-8'>
+                <div>ACTIVATE YOUR LP FARMING OR DEPOSIT LP TO START</div> 
+                <RoundedButton title='Activate'/>
+              </div>
+            </div>
            : (<></>)}
         </SlideDown>
       </Board>  
